715bb15e0714d07d7e07e66f004887bfee39a749
Beata von Grothusen <admin@Beatas-MacBook-Air-2.local>
2021-01-28 09:31:25 +0100

    hide markers on hover

--- ajaxLoadData.js
+++ ajaxLoadData.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
     var time_indicator = ''
     var vehicle_img = ""
     icon_type= ""
+    var previousNode = {}
+    var previousAction = ""
+    var upComingAction = ""
     var mymap = L.map('mapid').setView([57.467053 , 18.487117], 10);
+    var t = new Date();
+    var hour = t.getHours()
+    var minutes = t.getMinutes()
+    var seconds = t.getSeconds()
+    var time = hour + ':' + minutes + ':' + seconds
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -13,18 +21,33 @@ $(document).ready(function() {
         accessToken: 'pk.eyJ1IjoiYmVhdGF2b25nIiwiYSI6ImNrazg1OHp2MzBqOWUydm1oc2oxeTM2bTYifQ.VcuLIamxk3o81B6Y18HVzQ'
     }).addTo(mymap);
 
+    var greenIcon = L.icon({
+        iconUrl: 'images/sign-in-alt-solid.png',                
+        iconSize:     [25, 19]
+    });
+
+    var redIcon = L.icon({
+        iconUrl: 'images/sign-in-alt-solid-red.png',                
+        iconSize:     [25, 19]
+    });
+
 
     $.ajax({
         dataType: 'json',
         success: function(data) {
             $.each(data, function(key, value) {
-
+                var previousHalfHour = []
+                var nextHalfHour = []
+                var currentposition = [value.lat, value.long]
+                previousHalfHour.push(currentposition)
+                nextHalfHour.push(currentposition)
                 if (value.status === 'on-time') {
                     time_indicator = "<img src='images/green-oval.png' class='oval'>"
+                    indicator_strong = "<img src='images/dark-green-oval.png' class='oval'>"
                 }
                 if (value.status === 'late') {
                     time_indicator = "<img src='images/pink-oval.png' class='oval'>"
-                    console.log(time_indicator)
+                    indicator_strong = "<img src='images/red-oval.png' class='oval'>"
                 }
                 if (value.status === "") {
                     time_indicator = ""
@@ -36,37 +59,82 @@ $(document).ready(function() {
                     vehicle_img = "images/green-car.png"
                     icon_type = "images/green-marker.png"
                 }
+                if (value.upComingNodes[0].action = 'pickup') {
+                    upComingAction = "images/sign-in-alt-solid.png"
+                } else {
+                    upComingAction = "images/sign-in-alt-solid-red.png"
+                }
 
+                $.each(value.previousNodes, function(x, y) {
+                    var pos = [y.lat, y.long]
+                    previousHalfHour.push(pos)
+                    if (y.action === "dropoff") {
+                        var dropoff = L.marker(pos, {icon: redIcon}).addTo(mymap)
+                        $(dropoff._icon).addClass(value.car + 'marker polyline')
+                    }
+                    if (y.action === "pickup") {
+                        var pickup = L.marker(pos, {icon: greenIcon}).addTo(mymap)
+                        $(pickup._icon).addClass(value.car + 'marker polyline')
+
+                    }
+                    if (y.action !== 'coordinate') {
+                        previousNode = y
+                        if (y.action = 'dropoff') {
+                            previousAction = "images/sign-in-alt-solid-red.png"
+                        }
+                        if (y.action = 'pickup') {
+                            previousAction = "images/sign-in-alt-solid.png"
+                        }
+                        return false
+                    }                  
+                })
+
+                $.each(value.upComingNodes, function(x, y) {
+                    var pos = [y.lat, y.long]
+                    nextHalfHour.push(pos) 
+                    if (y.action === 'pickup') {
+                        var pickup = L.marker(pos, {icon: greenIcon}).addTo(mymap)
+                        $(pickup._icon).addClass(value.car + 'marker polyline')
+                    }
+                    if (y.action === 'dropoff') {
+                        var dropoff = L.marker(pos, {icon: redIcon}).addTo(mymap)
+                        $(dropoff._icon).addClass(value.car + 'marker polyline')
+                    }            
+                })
+
+                var polylinePrevious = L.polyline(previousHalfHour, {className: value.car+'polyline polyline', color: 'green', weight: 7}).addTo(mymap);  
+                var polylineNext = L.polyline(nextHalfHour, {className: value.car+'polyline polyline', color: 'green', weight: 7, dashArray: '3, 10', dashOffset: '0'}).addTo(mymap);    
+                
                 L.marker([value.lat, value.long], {icon: new L.DivIcon({
                     className: 'div-icon',
                     html: 
                     '<div class="hover-popup hide ' + value.car + '">' + 
                         '<div class="col-sm-12">' +
                             '<div class="row">' +
-                                '<p class="col-sm-9 text-popup bold">Senast uppdaterad: </p><p class="col-sm-3 text-popup green bold">07:29</p>' +
+                                '<p class="col-sm-9 text-popup bold">Senast uppdaterad: </p><p class="col-sm-3 text-popup green bold">'+ value.previousNodes[0].time + '</p>' +
                             '</div>' +
                             '<div class="row col-sm-12 list">' +
                                 '<div class="col-sm-1 time">' +
-                                    '<img src="images/dark-green-oval.png" class="oval" alt="">' +
+                                    indicator_strong +
                                 '</div>' +
                                 '<div class="col-sm-11">' +
-                                    '<p class="text-popup indicator">I tid</p>' +
+                                    '<p class="text-popup indicator">'+ value.time +'</p>' +
                                 '</div>' +
                             '</div>' +
                             '<div class="row col-sm-12 list">' +
                                 '<div class="col-sm-1 list">' +
-                                    '<img src="images/sign-in-alt-solid.png" class="oval" alt="">' +
+                                    '<img src=' + previousAction + ' class="oval" alt="">' +
                                 '</div>' +
                                 '<div class="col-sm-11">' + 
-                                    '<p class="text-popup indicator">07:11(+1) <br> Stångagatan, Visby</p>' + 
+                                    '<p class="text-popup indicator">' + previousNode.time + '<br>' + previousNode.address + '</p>' + 
                                 '</div>' +
                             '</div>' +
                             '<div class="row col-sm-12 list">' +
                                 '<div class="col-sm-1 list">' +
-                                    '<img src="images/sign-in-alt-solid-red.png" class="oval" alt="">' +
+                                    '<img src=' + upComingAction + ' class="oval" alt="">' +
                                 '</div>' +
                                 '<div class="col-sm-11">' +
-                                    '<p class="text-popup indicator">Sjonhem, Romakloster</p>' +
+                                    '<p class="text-popup indicator">' + value.upComingNodes[0].address + '</p>' +
                                 '</div>' +
                             '</div>' +
                             '<div class="row col-sm-12 list">' +
@@ -87,15 +155,17 @@ $(document).ready(function() {
                             '</div>' +                                               
                         '</div>' +
                     '</div>' +
-                    '<div class="marker" onmouseover="showPopup('+ value.car +');" onmouseout="removePopup('+ value.car +')";>' + 
+                    '<div id="marker-div'+ value.car +'" class="marker" onmouseover="showPopup('+ value.car +'); showPolyline('+ value.car +')" onmouseout="removePolyline('+ value.car +'); removePopup('+ value.car +')";>' + 
                         '<img class="div-image" src=' + icon_type + '>' +
                             '<div class="icon-text">' + value.car + '</div>' + 
                         '</img>' + 
                     '</div>'
                 })} ).addTo(mymap);
+
+                var detail=document.getElementById('detail')
                 
                 $("#drive-order-list").append(
-                    "<li class='car'>" + 
+                    "<li onclick='listClick()' class='car listitem" + value.car + "'>" + 
                         "<div class='row'>" + 
                             "<div class='car-div col-xs-3'>" + 
                                 "<img src=" + vehicle_img + " class='car-img'>" + 
@@ -126,6 +196,41 @@ $(document).ready(function() {
 })	
 
 
+function showPolyline(carNumber) {
+    var a = document.getElementsByClassName(carNumber.toString()+'polyline');
+    for (i = 0; i < a.length; i++) {
+        a[i].classList.remove('polyline')
+    }
+    var b = document.getElementsByClassName(carNumber.toString()+'marker');
+    for (i = 0; i < b.length; i++) {
+        b[i].classList.remove('polyline')
+    }
+    var c = document.getElementsByClassName('marker');
+    for (i = 0; i < c.length; i++) {
+        console.log(c[i].id)
+        if (c[i].id !== 'marker-div' + carNumber.toString()) {
+            c[i].classList.add('polyline')
+        }
+    }
+}
+
+function removePolyline(carNumber) {
+    var a = document.getElementsByClassName(carNumber.toString()+'polyline');
+    for (i = 0; i < a.length; i++) {
+        a[i].classList.add('polyline')
+    }
+    var b = document.getElementsByClassName(carNumber.toString()+'marker');
+    for (i = 0; i < b.length; i++) {
+        b[i].classList.add('polyline')
+    }
+    var c = document.getElementsByClassName('marker');
+    for (i = 0; i < c.length; i++) {
+        if (c[i].id !== 'marker-div' + carNumber.toString()) {
+            c[i].classList.remove('polyline')
+        }
+    }
+}
+
 function setActive(element, classname, activator) {
     var a = document.getElementsByClassName(classname);
     for (i = 0; i < a.length; i++) {
@@ -136,23 +241,31 @@ function setActive(element, classname, activator) {
 
 function showPopup(carNumber) {
     var a = document.getElementsByClassName(carNumber.toString());
+    var listobj = document.getElementsByClassName('listitem'+carNumber.toString());
     for (i = 0; i < a.length; i++) {
         a[i].classList.remove('hide')
+    }  
+    for (i = 0; i < listobj.length; i++) {
+        listobj[i].classList.add('grey')
     }
 }
 
 function removePopup(carNumber) {
     var a = document.getElementsByClassName(carNumber.toString());
+    var listobj = document.getElementsByClassName('listitem'+carNumber.toString());
     for (i = 0; i < a.length; i++) {
         a[i].classList.add('hide')
     }
-    console.log("Mouse out!")
+    for (i = 0; i < listobj.length; i++) {
+        listobj[i].classList.remove('grey')
+    }
 }
 
-function getDo() {
-    var today = new Date
-
-} 
+function listClick() {
+    setActive(document.getElementById('detail'), 'view', 'current')
+    setActive(document.getElementById('second'), 'menu-item', 'active')
+    setActive(document.getElementById('text-detail'), 'text-menu', 'text-active')
+}
 	// DO GET
 	// function specialVehicleAlertGet(){
     //     var t = new Date();
